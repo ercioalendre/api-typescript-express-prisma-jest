1caa6613d1df1a06e533b8b4ced501678bc8a062
Ercio Alendre <ercio.alendre@gmail.com>
2021-11-01 01:06:42 -0300

    CreateOneUser repository was created to create new users

---
+++ src/modules/users/repositories/CreateOneUser.repository.ts
@@ -0,0 +1,27 @@
+import { PrismaClient, User } from "@prisma/client";
+import { hash } from "bcryptjs";
+import { IUser } from "../interfaces/IUser.interface";
+
+const prisma = new PrismaClient();
+const users = prisma.user;
+
+export default class CreateOneUser {
+  static async execute({ full_name, email, phone, password }: IUser): Promise<User | null> {
+    const hashedPassword = await hash(password, 8);
+
+    const data = {
+      full_name,
+      email,
+      phone,
+      password: hashedPassword,
+    };
+
+    try {
+      return await users.create({
+        data,
+      });
+    } catch (error) {
+      throw new Error(error as string);
+    }
+  }
+}

d4c584e65c4c77aabc4089390fb53d991f058de2
Ercio Alendre <ercio.alendre@gmail.com>
2021-11-02 02:15:29 -0300

    refactor: variable usersRoutes renamed to UsersRoutes to meet convention

--- src/modules/users/routes/Users.routes.ts
+++ src/modules/users/routes/Users.routes.ts
@@ -4,9 +4,9 @@ import CreateUserController from "@modules/users/controllers/CreateUser.controll
 import CreateUserSessionController from "@modules/users/controllers/CreateUserSession.controller";
 import checkLoginUserForm from "@shared/http/middlewares/checkLoginUserForm.middleware";
 
-const usersRoutes = Router();
+const UsersRoutes = Router();
 
-usersRoutes.post("/", checkLoginUserForm, CreateUserSessionController.execute);
-usersRoutes.post("/users/create", checkNewUserForm, CreateUserController.execute);
+UsersRoutes.post("/", checkLoginUserForm, CreateUserSessionController.execute);
+UsersRoutes.post("/users/create", checkNewUserForm, CreateUserController.execute);
 
-export default usersRoutes;
+export default UsersRoutes;

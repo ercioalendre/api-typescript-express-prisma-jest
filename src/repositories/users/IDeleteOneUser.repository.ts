251a03735dc5c38ce3a5432be4596d106cc22d5e
Ercio Alendre <ercio.alendre@gmail.com>
2021-11-04 14:35:56 -0300

    IDeleteOneUser.repository.ts created

---
+++ src/repositories/users/IDeleteOneUser.repository.ts
@@ -0,0 +1,6 @@
+import { User } from "@entities/User.entity";
+import IUserUniqueFieldsDto from "@requirements/dto/users/IUserUniqueFields.dto";
+
+export interface IDeleteOneUserRepository {
+  execute(id: IUserUniqueFieldsDto): Promise<User | string | null>;
+}

aa297ec29d3227896b9acee267b9d6b06d5012a8
Ercio Alendre <ercio.alendre@gmail.com>
2021-11-04 18:39:35 -0300

    refactor: SocialMedias attribute added

--- src/requirements/dto/users/IUser.dto.ts
+++ src/requirements/dto/users/IUser.dto.ts
@@ -4,4 +4,12 @@ export default interface IUserDto {
   email: string;
   phone: string;
   password: string;
+  SocialMedias?: {
+    id?: string;
+    facebook?: string;
+    twitter?: string;
+    instagram?: string;
+    youtube?: string;
+    linkedin?: string;
+  };
 }

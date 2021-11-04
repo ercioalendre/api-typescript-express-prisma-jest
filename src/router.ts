594a7374b74ee90faca6d743c811f4304250cbbf
Ercio Alendre <ercio.alendre@gmail.com>
2021-11-04 01:30:39 -0300

    routes.ts renamed to router.ts

--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,9 @@
 import express from "express";
-import usersRoutes from "@modules/users/routes/Users.routes";
+import UsersRoutes from "@requirements/users/routers/Users.router";
 
 const Router = express.Router();
 
-Router.use("/", usersRoutes);
+Router.use("/", UsersRoutes);
 Router.use(express.static("public"));
 Router.get("*", (req, res) => {
   res.status(404).json("Oops! Page not found.");

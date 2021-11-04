9173af525b52a189b72842ff23d0bab4ac0164b5
Ercio Alendre <ercio.alendre@gmail.com>
2021-11-04 18:37:32 -0300

    refactor: return type of the method execute updated

--- src/repositories/users/ICreateOneUser.repository.ts
+++ src/repositories/users/ICreateOneUser.repository.ts
@@ -1,6 +1,6 @@
-import { User } from "@entities/User.entity";
+import { User as PrismaUserObject } from "@prisma/client";
 import IUserDto from "@requirements/dto/users/IUser.dto";
 
 export interface ICreateOneUserRepository {
-  execute(data: IUserDto): Promise<User | null>;
+  execute(data: IUserDto): Promise<PrismaUserObject | null>;
 }

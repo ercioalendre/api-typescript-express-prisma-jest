b26fd5d0cbca26ee345f20de58bf7152d0d5b060
Ercio Alendre <ercio.alendre@gmail.com>
2021-11-04 14:32:10 -0300

    GetOneUser.useCase.ts created

---
+++ src/requirements/users/useCases/GetOneUser.useCase.ts
@@ -0,0 +1,25 @@
+import AppError from "@components/errors/AppError";
+import { User } from "@entities/User.entity";
+import GetOneUserRepository from "@repositories/users/implementations/prisma/GetOneUser.repository";
+import IUserUniqueFieldsDto from "@requirements/dto/users/IUserUniqueFields.dto";
+
+export default class GetAllUsersUseCase {
+  private getOneUserRepository: GetOneUserRepository;
+
+  constructor() {
+    this.getOneUserRepository = new GetOneUserRepository();
+  }
+
+  async execute(id: IUserUniqueFieldsDto): Promise<User | null | void> {
+    const user = await this.getOneUserRepository.execute(id);
+
+    if (user) {
+      return user;
+    } else {
+      throw new AppError({
+        message: "Nenhum usuário encontrado.",
+        statusCode: 400,
+      });
+    }
+  }
+}
